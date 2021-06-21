f73ad69af64ee053d09ba84886abefb9896c2907
Vinicios Neves <vinicios@gipsydanger.local>
2021-06-21 09:41:44 -0300

    get / post / delete do TODO list

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Formulario from "./Formulario";
 import Tarefas from "./Tarefas";
 import Item from "./Item";
+import axios from "axios";
 
 function App() {
 
   const [tarefas, setTarefas] = useState([])
+
+  const obterTarefas = () => {
+    axios.get('http://localhost:8000/tarefas')
+      .then( response => setTarefas(response.data) )
+      .catch( erro => console.log(erro) )
+  }
+  
+  useEffect(() => {
+    obterTarefas()
+  }, [])
+
   const adicionarTarefa = (tarefa) => {
-    setTarefas([
-      tarefa,
-      ...tarefas
-    ])
+    // {
+    //   tarefa: `tarefa`,
+    //   prazo: `prazo`
+    // }
+    axios.post('http://localhost:8000/tarefas', tarefa)
+      .then(response => {
+        const novaTarefa = response.data
+        setTarefas([
+          novaTarefa,
+          ...tarefas
+        ])
+      })
   }
 
-  const excluir = (xpto) => {
-    tarefas.splice(xpto, 1)
-    setTarefas([
-      ...tarefas
-    ])
+  const excluir = (id) => {
+    axios.delete('http://localhost:8000/tarefas/' + id)
+      .then(() => {
+        obterTarefas()
+      })
   }
 
   return (
     <div>
       <Formulario aoSalvar={adicionarTarefa}/>
       <Tarefas>
-        {tarefas.map((item, indice) => 
-          // <li key={indice}>
-          //   {item.tarefa} - [ <a href="/#" onClick={() => { excluir(indice) }}>excluir</a> ]
-          // </li>
-        <Item key={indice} excluir={excluir} indice={indice} item={item.tarefa}/>
-        
+        {tarefas.map((item) => 
+        <Item key={item.id} excluir={excluir} id={item.id} item={item.tarefa}/>        
         )}
       </Tarefas>
     </div>
--- src/Item/index.jsx
+++ src/Item/index.jsx
@@ -1,12 +1,20 @@
 const Item = (props) => {
 
   const solicitarExclusao = () => {
-    props.excluir(props.indice)
+    props.excluir(props.id)
+  }
+
+  const solicitarEdicao  = () => {
+    props.editar({
+      id: props.id,
+      prazo: props.prazo,
+      tarefa: props.item,
+    })
   }
 
   return (
     <li>
-      {props.item} - [ <a href="/#" onClick={solicitarExclusao}>excluir</a> ]
+      {props.item} - [ <a href="/#" onClick={solicitarEdicao}>editar</a> ] - [ <a href="/#" onClick={solicitarExclusao}>excluir</a> ]
     </li>
   )
 }
